5f67152e6256e43b9fbcfef44e3232572b13a286
Rai <raihana.noori2026@gbwl.org>
2025-03-03 17:11:48 +0000

    starting JS

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("hello world");
+/* this is a MULTI-LINE comment in JS 
+Below is an example of a JS STATEMENT (instruction/command)
+Statements end with a SEMICOLIN (;)
+*/
+console.log("Hey!");
+
+
+//This is a SINGLE-LINE comment
+// console. Log is a FUNCTION to display information
+console.log("this is the console/output")
+
+//DECLEAR a VERIABLE to hold data
+//Use the let keyword to create the variable
+let pokemon;
+
+//ASSION a value to that variable 
+//like packing an item into a box
+pokemon = "Pikachu" ;
+
+
+//Can DECLEAR and ASSOIN in one statement 
+let name = "Rai" ;
