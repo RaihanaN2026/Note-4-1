fc0e7ac1ac981ff9c7319177b960f7943fac6759
Rai <raihana.noori2026@gbwl.org>
2025-03-05 17:10:03 +0000

    adding more comments to the JS

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-/* this is a MULTI-LINE comment in JS 
+/* This is a MULTI-LINE comment in JS 
 Below is an example of a JS STATEMENT (instruction/command)
-Statements end with a SEMICOLIN (;)
+Statements end with a SEMICOLON (;)
 */
 console.log("Hey!");
 
@@ -9,14 +9,29 @@ console.log("Hey!");
 // console. Log is a FUNCTION to display information
 console.log("this is the console/output")
 
-//DECLEAR a VERIABLE to hold data
+//DECLEAR a VARIABLE to hold data
 //Use the let keyword to create the variable
 let pokemon;
 
-//ASSION a value to that variable 
+//ASSIGN a value to that variable 
 //like packing an item into a box
 pokemon = "Pikachu" ;
 
 
 //Can DECLEAR and ASSOIN in one statement 
 let name = "Rai" ;
+
+// DECLEAR a new variable (buying a box)
+let luckynumber;
+
+// ASSIGN a value to that variable 
+// (putting an item in a box)
+luckynumber= 7;
+
+//DECLEAD and ASSIGN in one statement
+let worstnumber= 22;
+
+// You can "PASS IN" a VARIABLE into a FUNCTION
+console.log(luckynumber);
+
+console.log(worstnumber);
